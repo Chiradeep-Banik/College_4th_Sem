1a4de9455d83616f4bf5b5fe32c6c386f641d21b
chiradeep <chiradeep13@gmail.com>
2022-04-17 03:48:04 +0530

    Learned some neew concepts

--- OOP-Class/template.cpp
+++ OOP-Class/template.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+#include <vector>
 
+template <typename T1, typename T2>
+void print(T1 a, T2 b)
+{
+    std::cout << a << " " << b << std::endl;
+}
+
+template <typename T>
+class node
+{
+private:
+    T data;
+    node<T> *next;
+
+public:
+    node<T>(T val)
+    {
+        this->data = val;
+        this->next = NULL;
+        std::cout << "node created" << std::endl;
+    }
+    void print()
+    {
+        std::cout << data << std::endl;
+    }
+    virtual ~node()
+    {
+        std::cout << "node deleted" << std::endl;
+    }
+};
 int main()
 {
+    print<int, std::string>(1, "hello");
+    node<int> n1 = node<int>(1);
+    node<std::string> n2 = node<std::string>("asd");
+
+    n1.print();
+    n2.print();
+    return 0;
 }
